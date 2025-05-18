acb6d508317bfe799de1d323207d83ec7992e393
FunkyHorns <lancejoshua.hilario.cics@ust.edu.ph>
2025-05-18 16:07:10 +0800

    Working Local Login Logout

--- finallab/src/main.jsx
+++ finallab/src/main.jsx
@@ -4,18 +4,19 @@ import { createRoot } from "react-dom/client";
 import "../src/index.css";
 import App from "./App.jsx";
 import { UserProvider } from "./Signup/UserContext";
-
-const CLIENT_ID =
-  "658901072922-9jt0qlj7ark87v701khufsrvbpkkroi1.apps.googleusercontent.com";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const CLIENT_ID =
   "658901072922-9jt0qlj7ark87v701khufsrvbpkkroi1.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      {/* <App /> */}
     <UserProvider>
       <App />
     </UserProvider>
     ;
+    </GoogleOAuthProvider>
   </StrictMode>
 );
