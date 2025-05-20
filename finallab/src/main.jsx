bb2cd215f3bbbd0e1944d2d08ddea36f097d4b4a
FunkyHorns <142652375+BonafideMona@users.noreply.github.com>
2025-05-20 23:50:21 +0800

    deleted maps_ID in main.jsx

--- finallab/src/main.jsx
+++ finallab/src/main.jsx
@@ -11,9 +11,6 @@ import { CartProvider } from "./contexts/CartContext";  // <-- import your CartP
 const CLIENT_ID =
   "658901072922-9jt0qlj7ark87v701khufsrvbpkkroi1.apps.googleusercontent.com";
 
-  // Initialize Google Maps API
-  const MAPS_ID=
-  "AIzaSyBSgJ8YkO7z94BQvUMufPiQ7WU8EeI9nL0";
 
 
 createRoot(document.getElementById("root")).render(
